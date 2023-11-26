402758939e061be671e918cc33971cccc43f4626
“Salar <salar.sali97@gmail.com>
2023-11-27 01:06:34 +0300

    notification icon implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap";
 import HomePage from "./pages/home-page";
 import StorePage from "./pages/store-page";
 import AboutPage from "./pages/about-page";
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/store" element={<StorePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Container>
+    <>
+      <AppNavbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/store" element={<StorePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/utilities/config.ts
@@ -0,0 +1,5 @@
+export enum routes {
+  HOME = "/",
+  STORE = "/store",
+  ABOUT = "/about",
+}
