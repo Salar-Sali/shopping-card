4eaa998b189d2ef26ce2febc0865ce4b5cb81978
“Salar <salar.sali97@gmail.com>
2024-01-06 02:18:05 +0300

    localization button added

--- src/components/app-navbar/app-navbar.tsx
+++ src/components/app-navbar/app-navbar.tsx
@@ -4,6 +4,7 @@ import { routes } from "../../utilities/config";
 import { useTranslation } from "react-i18next";
 import langKey from "../../bootstrap/i18n/langKey";
 import { useShoppingCartContext } from "../../context/shopping-cart-context/shopping-cart-context";
+import AppNavbarLocalizationButton from "./localization-button/app-navbar-localization-button";
 
 const AppNavbar = () => {
   const { t } = useTranslation();
@@ -23,20 +24,23 @@ const AppNavbar = () => {
             {t(langKey.navbar.about)}
           </Nav.Link>
         </Nav>
-        {cartQuantity > 0 && (
-          <Button
-            onClick={openCart}
-            style={{ width: "3rem", height: "3rem", position: "relative" }}
-            variant="outline-primary"
-            className="rounded-circle "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-              fill="currentColor"
+        <div className="d-flex align-items-center gap-2">
+          <AppNavbarLocalizationButton />
+          {/* quantity */}
+          {cartQuantity > 0 && (
+            <Button
+              onClick={openCart}
+              style={{ width: "3rem", height: "3rem", position: "relative" }}
+              variant="outline-primary"
+              className="rounded-circle "
             >
-              <path
-                d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                fill="currentColor"
+              >
+                <path
+                  d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 
             32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 
             278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 
             360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14
@@ -44,25 +48,26 @@ const AppNavbar = () => {
              416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 
              128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416
               464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"
-              />
-            </svg>
-            <div
-              className="bg-danger rounded-circle 
+                />
+              </svg>
+              <div
+                className="bg-danger rounded-circle 
                       d-flex justify-content-center align-items-center"
-              style={{
-                width: "1.5rem",
-                height: "1.5rem",
-                position: "absolute",
-                right: 0,
-                bottom: 0,
-                transform: "translate(25%,25%)",
-                color: "white",
-              }}
-            >
-              {cartQuantity}
-            </div>
-          </Button>
-        )}
+                style={{
+                  width: "1.5rem",
+                  height: "1.5rem",
+                  position: "absolute",
+                  right: 0,
+                  bottom: 0,
+                  transform: "translate(25%,25%)",
+                  color: "white",
+                }}
+              >
+                {cartQuantity}
+              </div>
+            </Button>
+          )}
+        </div>
       </Container>
     </Navbar>
   );
---
+++ src/components/app-navbar/localization-button/app-navbar-localization-button.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { LANGS } from "../../../bootstrap/i18n/init-i18n";
+
+const AppNavbarLocalizationButton = () => {
+  const { i18n } = useTranslation();
+  const [lang, setLang] = useState<LANGS>(LANGS.EN);
+  const handleSetLanguage = (language: LANGS) => {
+    setLang(language);
+    i18n.changeLanguage(language);
+  };
+
+  const langs = [LANGS.EN, LANGS.RU, LANGS.AR];
+
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-danger dropdown-toggle"
+        data-bs-toggle="dropdown"
+      >
+        {lang}
+      </button>
+      <ul className="dropdown-menu" style={{ minWidth: "20px" }}>
+        {langs.map((lang) => {
+          return (
+            <li>
+              <button
+                className="dropdown-item btn-xs"
+                onClick={() => handleSetLanguage(lang)}
+              >
+                {lang}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default AppNavbarLocalizationButton;
